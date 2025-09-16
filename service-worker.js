faf1326070dc6cf0194cbd57bb4df6aa311f5b26
Sayan Mondal <88304103+sayan8bit@users.noreply.github.com>
2025-09-16 08:32:37 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,20 @@
-const CACHE_NAME = "pwa-cache-v1";
-const FILES_TO_CACHE = [
+const CACHE_NAME = "my-pwa-cache-v1";
+const ASSETS = [
   "./",
   "./index.html",
+  "./manifest.json",
   "./icon-192.png",
-  "./icon-512.png",
+  "./icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
